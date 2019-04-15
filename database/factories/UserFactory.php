a736e921460535abda7f372fbc78014c55924e6f
prof-anis <emeritusprofessoranis@gmail.com>
2019-04-15 13:21:52 +0100

    the therapist table has been setup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,21 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'role'=>'therapist',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\therapist::class, function (Faker $faker) {
+    return [
+       'location'=>$faker->address,
+       'age'=>rand(18,60),
+       'rating'=>rand(0,5),
+       'fee_per_hour'=>rand(10000,20000),
+       'years_of_experience'=>rand(2,10),
+       'availability'=>true,
+
+       'user_id'=>App\User::all()->random()->id
     ];
 });
