e7ce27511c0334d13d97ba3b013f86380b1c2df5
kevchike <kevchike@gmail.com>
2019-04-15 21:01:29 +0100

    Fetch user detail

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,13 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('/auth/register',"API\AuthController@register");
 
 Route::post('/auth/login',"API\AuthController@login");
 
 Route::post('/user-details',"API\AuthController@details");
+
+Route::get('/user/{userId}',"API\UserController@getSingleUser");
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
