0e335af78b740aaeebd99cb83aea6c91fd2c86a3
AlfredBryan <alfred.chimereze@gmail.com>
2019-04-16 15:08:09 +0100

    checking pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
-import Become from "./Components/become-a-therapist";
+import Become from "./Components/BecomePage/Become";
+import Contact from "./Components/Contact/Contact";
+import Faq from "./Components/Faq/Faq";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
         <Switch>
-          <Route exact path="/" component={H} />
-          <Route exact path="/login" component={L} />
-          <Route exact path="/register" component={R} />
+          <Route exact path="/" component={Become} />
+          {/*<Route exact path="/" component={H} /> */}
+          {/*<Route exact path="/register" component={R} />
           <Route exact path="/profile" component={} />
-          <Route exact path="/become-a-therapist" component={Become} />
+          <Route exact path="/become-a-therapist" component={Become} />*/}{" "}
         </Switch>
       </div>
     );
