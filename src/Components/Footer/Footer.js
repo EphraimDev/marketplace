4284f75e30d5b72c0eaf3dbfafd80d23300cfdd4
Peerichie <peerichie9@gmail.com>
2019-04-17 14:01:59 +0100

    Added copyrights and footer link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -192,10 +192,25 @@ class Footer extends Component {
               <hr />
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-                <p class="list-unstyled list-inline social text-center">
+                <p class="list-unstyled list-inline text-left">
                 Copyright &copy;{new Date().getFullYear()} All rights reserved |
                 TherapyApp
               </p>
+              <div class="col-xs-12 col-sm-3 col-md-3"></div>
+                <ul class="list-unstyled quick-links text-center">
+                    <li class="list-inline-item">
+                      <a href="javascript:void();">
+                        <i class="fa fa-angle-double-right" />
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="javascript:void();">
+                        <i class="fa fa-angle-double-right" />
+                        Terms and Condition
+                      </a>
+                    </li>
+                  </ul>
             </div>
           </div>
         </section>
