99dac21956d9cf0ae8ae68e518db27b82e12d504
AlfredBryan <alfred.chimereze@gmail.com>
2019-04-17 12:54:57 +0100

    made changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -190,23 +190,7 @@ class Footer extends Component {
             </div>
             <div class="row">
               <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                <p>
-                  <u>
-                    <a href="#">National Transaction Corporation</a>
-                  </u>{" "}
-                  is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly
-                  owned subsidiary of U.S. Bancorp, Minneapolis, MN]
-                </p>
-                <p class="h6">
-                  &copy All right Reversed.
-                  <a
-                    class="text-green ml-2"
-                    href="#"
-                    target="_blank"
-                  >
-                    Sunlimetech
-                  </a>
-                </p>
+               
               </div>
               <hr />
             </div>
