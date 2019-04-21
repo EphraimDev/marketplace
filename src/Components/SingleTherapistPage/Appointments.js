a8aca44c61a582b81b3b95505600d36a012805a1
mrphemi <mrlapinho@gmail.com>
2019-04-21 05:28:14 +0100

    Work on single therapist page

---
+++ src/Components/SingleTherapistPage/Appointments.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Appointments = props => {
+	return (
+		<div className="card">
+			<h5 className="card-title text-center">Appointments</h5>
+		</div>
+	);
+};
+
+export default Appointments;
