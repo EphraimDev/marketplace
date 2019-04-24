096b2c57a6a8fbbdac17a04776a3e969bf056d79
neydoo <enoch4real7@gmail.com>
2019-04-24 20:58:36 +0100

    activated Protected Route for /user

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -7,7 +7,7 @@ class ProtectedRoute extends Component {
     render() {
         const Component = this.props.component;
     return (<Route {...this.rest} render={() => (
-        token !== null
+        token 
           ? <Component {...this.props} />
             : <Redirect to='/login' />
       )} />)
